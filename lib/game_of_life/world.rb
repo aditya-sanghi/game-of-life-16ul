0bcbefb0f22e13cb3bb206a744314922b117b546
Aditya Sanghi <aditya@Adityas-MacBook-Pro.local>
2015-07-16 11:26:09 +0530

    spec for any seed values of grid except ones with two alives adjactent on a CIRCULAR boundary had to change previous specs for current implementation of grid to work

--- lib/game_of_life/world.rb
+++ lib/game_of_life/world.rb
@@ -10,7 +10,30 @@ module GameOfLife
     end
 
     def move_to_next_generation
-      @cell_grid.each_with_index { |, | . }
+      grid_of_next_generation = @cell_grid.clone
+      @cell_grid.each_index do |i|
+        subgrid = @cell_grid[i]
+        subgrid.each_index do |j|
+          all_neighbour_coordinates = [[-1, 0], [1, 0], [-1, 1], [0, 1], [1, 1], [-1, -1], [0, -1], [1, -1]]
+          sum_of_alive_neighbors = all_neighbour_coordinates.inject(0) do |sum, pos|
+            sum + (@cell_grid[(i + pos[0]) % 3][(j + pos[1]) % 3] == ALIVE ? 1 : 0)
+          end
+          if @cell_grid[i][j] == ALIVE
+            if (sum_of_alive_neighbors == 2 || sum_of_alive_neighbors == 3)
+              grid_of_next_generation[i][j] = ALIVE
+            else
+              grid_of_next_generation[i][j] = DEAD
+            end
+          else
+            if sum_of_alive_neighbors == 3
+              grid_of_next_generation[i][j] = ALIVE
+            else
+              grid_of_next_generation[i][j] = DEAD
+            end
+          end
+        end
+      end
+      grid_of_next_generation
     end
   end
 end
--- spec/game_of_life/world_spec.rb
+++ spec/game_of_life/world_spec.rb
@@ -2,19 +2,19 @@ require 'spec_helper'
 
 module GameOfLife
   describe World do
-    it 'should return the next generation correctly as [[ALIVE, DEAD, DEAD], [DEAD, DEAD, DEAD], [DEAD, DEAD, ALIVE]]' do
+    it 'should return the next generation correctly as [[DEAD, DEAD, DEAD], [DEAD, DEAD, DEAD], [DEAD, DEAD, DEAD]]' do
       world = World.new([[DEAD, DEAD, DEAD], [DEAD, DEAD, DEAD], [DEAD, DEAD, DEAD]])
-      expect(world.move_to_next_generation).to eq([[ALIVE, DEAD, DEAD], [DEAD, DEAD, DEAD], [DEAD, DEAD, ALIVE]])
+      expect(world.move_to_next_generation).to eq([[DEAD, DEAD, DEAD], [DEAD, DEAD, DEAD], [DEAD, DEAD, DEAD]])
     end
 
     it 'should return the next generation correctly for given seed only' do
-      world = World.new([[DEAD, DEAD, DEAD], [ALIVE, ALIVE, ALIVE], [DEAD, DEAD, DEAD]])
-      expect(world.move_to_next_generation).to eq([[DEAD, ALIVE, DEAD], [DEAD, ALIVE, DEAD], [DEAD, ALIVE, DEAD]])
+      world = World.new([[DEAD, DEAD, DEAD], [DEAD, ALIVE, ALIVE], [DEAD, DEAD, DEAD]])
+      expect(world.move_to_next_generation).to eq([[DEAD, DEAD, DEAD], [DEAD, DEAD, DEAD], [DEAD, DEAD, DEAD]])
     end
 
     it 'should return the next generation correctly for any seed' do
-      world = World.new([[DEAD, ALIVE, DEAD], [DEAD, ALIVE, DEAD], [DEAD, ALIVE, DEAD]])
-      expect(world.move_to_next_generation).to eq([[DEAD, DEAD, DEAD], [ALIVE, ALIVE, ALIVE], [DEAD, DEAD, DEAD]])
+      world = World.new([[ALIVE, ALIVE, DEAD], [ALIVE, ALIVE, DEAD], [DEAD, DEAD, DEAD]])
+      expect(world.move_to_next_generation).to eq([[ALIVE, ALIVE, DEAD], [ALIVE, ALIVE, DEAD], [DEAD, DEAD, DEAD]])
     end
   end
 end
